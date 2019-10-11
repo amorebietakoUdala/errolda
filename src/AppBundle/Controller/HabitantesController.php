05c63e8e0b57008efdb61f26f577ff3e913c6aa4
Amorebietako Udala <informatika@amorebieta.eus>
2019-10-11 08:05:34 +0200

    - Kontsulta zerbitzuan, "Udalerri honetan" zutabean, alta datarik   ez daukagunean, 1996a baino lehen izan dela atera.

--- src/AppBundle/Controller/HabitantesController.php
+++ src/AppBundle/Controller/HabitantesController.php
@@ -13,62 +13,66 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Services\ErroldaService;
 use AppBundle\Forms\HabitanteBilatzaileaForm;
-use AppBundle\Entity\Habitante;
-use AppBundle\Entity\Vivienda;
 use AppBundle\Utils\Balidazioak;
 
 /**
- * Description of ErroldaTxartelaController
+ * Description of ErroldaTxartelaController.
  *
  * @author ibilbao
 
-/**
-* @Route("/{_locale}/biztanleak")
-*/
-
-class HabitantesController extends Controller {
-
-     /**
+ /**
+ * @Route("/{_locale}/biztanleak")
+ */
+class HabitantesController extends Controller
+{
+    /**
      * @Route("/", name="biztanleak_search", options={"expose" = true})
      */
-    public function listAction (Request $request, ErroldaService $erroldaService){
-//	dump($request->getSession(),$request->getLocale());die;
-	$user = $this->get('security.token_storage')->getToken()->getUser();
-	$em = $this->getDoctrine()->getManager();
-	$bilatzaileaForm = $this->createForm(HabitanteBilatzaileaForm::class, [
+    public function listAction(Request $request, ErroldaService $erroldaService)
+    {
+        //	dump($request->getSession(),$request->getLocale());die;
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+        $em = $this->getDoctrine()->getManager();
+        $bilatzaileaForm = $this->createForm(HabitanteBilatzaileaForm::class, [
 //	    'role' => $user->getRoles(),
 //	    'locale' => $request->getLocale(),
-	]);
-	$bilatzaileaForm->handleRequest($request);
-	if ( $bilatzaileaForm->isSubmitted() && $bilatzaileaForm->isValid() ) {
-	    $data = $bilatzaileaForm->getData();
-	    if ( $data[ 'numDocumento'] === null &&  ( $data[ 'nombre'] === null || $data[ 'apellido1'] === null ) ) {
-		$this->addFlash(
-		    'error',
-		    'nana_edo_izena_eta_lehenengo_abizena_sartu_behar_dira'
-		);
-	    } else {
-		$data['numDocumento'] = Balidazioak::getDNIZenbakia($data['numDocumento']);
-		$consulta_habitante = $this->_remove_blank_filters($data);
-		$emaitza = $erroldaService->listAction($request, $consulta_habitante, $user);
-		return $this->render('/habitantes/search.html.twig', [
-		    'emaitza' => $emaitza,
-		    'bilatzaileaForm' => $bilatzaileaForm->createView(),
-		]);
-	    }
-	}
-	return $this->render('/habitantes/search.html.twig', [
-	    'bilatzaileaForm' => $bilatzaileaForm->createView()
-	]);
+    ]);
+        $bilatzaileaForm->handleRequest($request);
+        if ($bilatzaileaForm->isSubmitted() && $bilatzaileaForm->isValid()) {
+            $data = $bilatzaileaForm->getData();
+            if (null === $data['numDocumento'] && (null === $data['nombre'] || null === $data['apellido1'])) {
+                $this->addFlash(
+            'error',
+            'nana_edo_izena_eta_lehenengo_abizena_sartu_behar_dira'
+        );
+            } else {
+                $data['numDocumento'] = Balidazioak::getDNIZenbakia($data['numDocumento']);
+                $consulta_habitante = $this->_remove_blank_filters($data);
+                $emaitza = $erroldaService->listAction($request, $consulta_habitante, $user);
+//                dump($emaitza);
+//                die;
+
+                return $this->render('/habitantes/search.html.twig', [
+            'emaitza' => $emaitza,
+            'bilatzaileaForm' => $bilatzaileaForm->createView(),
+        ]);
+            }
+        }
+
+        return $this->render('/habitantes/search.html.twig', [
+        'bilatzaileaForm' => $bilatzaileaForm->createView(),
+    ]);
     }
 
-    private function _remove_blank_filters ($criteria) {
-	$new_criteria = [];
-	foreach ($criteria as $key => $value) {
-	    if (!empty($value))
-		$new_criteria[$key] = $value;
-	}
-	return $new_criteria;
-    }
+    private function _remove_blank_filters($criteria)
+    {
+        $new_criteria = [];
+        foreach ($criteria as $key => $value) {
+            if (!empty($value)) {
+                $new_criteria[$key] = $value;
+            }
+        }
 
+        return $new_criteria;
+    }
 }
